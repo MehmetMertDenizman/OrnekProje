d3464b63f0105c4c74d322e8d8b124b20e4db976
Mehmet Mert Denizman <amorylorch@gmail.com>
2022-10-16 17:07:25 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace OrnekProje
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("ANAN");
+                });
+
+            });
+
+        }
+
+    }
+}
